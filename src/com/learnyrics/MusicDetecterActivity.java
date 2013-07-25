cb1ccd2d1b62ee6e1c75d6ee8ab082431db4aa08
Vitor Teixeira <vitorfox@gmail.com>
2013-07-25 12:50:48 -0300

    Add activity to check music being played

---
+++ src/com/learnyrics/MusicDetecterActivity.java
@@ -0,0 +1,43 @@
+package com.learnyrics;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+public class MusicDetecterActivity extends Activity {
+	public static final String SERVICECMD = "com.android.music.musicservicecommand";
+	public static final String CMDNAME = "command";
+	public static final String CMDTOGGLEPAUSE = "togglepause";
+	public static final String CMDSTOP = "stop";
+	public static final String CMDPAUSE = "pause";
+	public static final String CMDPREVIOUS = "previous";
+	public static final String CMDNEXT = "next";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_music_detecter);
+		IntentFilter iF = new IntentFilter();
+		iF.addAction("com.android.music.metachanged");
+		iF.addAction("com.android.music.playstatechanged");
+		iF.addAction("com.android.music.playbackcomplete");
+		iF.addAction("com.android.music.queuechanged"); 
+		registerReceiver(mReceiver, iF);
+	}
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() { @Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			String cmd = intent.getStringExtra("command");
+			Log.i("Music", action + " / " + cmd);
+			String artist = intent.getStringExtra("artist");
+			String album = intent.getStringExtra("album");
+			String track = intent.getStringExtra("track");
+			Log.i("Music",artist+":"+track);
+		}
+	};
+}
